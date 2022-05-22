230725b1efb2693c71f0c648e9cc2090cc2dc449
Кристина Матулевская <matulevskaakristina@gmail.com>
2022-05-22 23:35:30 +0300

    Добавьте файлы проекта.

---
+++ МатулевскаяПР17/МатулевскаяПР17/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace МатулевскаяПР17
+{
+ 
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+    }
+}
---
+++ МатулевскаяПР17/МатулевскаяПР17/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace МатулевскаяПР17
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                lable1.TextColor = Color.Black;
+                string req = webClient.DownloadString($"http://numbersapi.com/{Number.Text.Replace(".", "")}?notfound=ceil");
+                lable1.Text = Translate(req);
+            }
+        }
+
+        public string Translate(string word)
+        {
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={HttpUtility.UrlEncode(word)}";
+            var webClient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var result = webClient.DownloadString(url);
+            try
+            {
+                result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                return result;
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+    }
+}
